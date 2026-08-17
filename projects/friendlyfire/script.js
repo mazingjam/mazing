2a33ff5d1d91bd07b0cad976604e5063fec2c341
Mr Maisey <mr.maisey@gmail.com>
2026-08-17 19:39:50 +0200

    Remove FriendlyFire from public site

--- projects/friendlyfire/script.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('form').addEventListener('submit', (event) => {
-  event.preventDefault();
-  const button = event.currentTarget.querySelector('button');
-  button.textContent = 'You are on the list!';
-  button.disabled = true;
-});
